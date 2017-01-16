8520cfafa1b3c93b13c94e8f2aa30e63e8c14911
peder280370 <peder@carolus.dk>
2017-01-16 11:26:07 +0100

    More work on public API documentation

---
+++ presentation/generate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DOCUMENT=presentation.adoc
+
+if [ "$1" = "pdf" ]; then
+   asciidoctor-pdf $DOCUMENT -D . -a imagesdir=images/
+   exit
+elif [ "$1" = "html" ]; then
+   asciidoctor $DOCUMENT -D .
+   exit
+else
+	echo Unknown target: "$1"
+	echo Valid targets are:
+fi
+
+echo "  pdf        Generates documentation in pdf"
+echo "  html       Generates documentation in html"
+
